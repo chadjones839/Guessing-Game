5679d149943ebc745cd28353535718b81f4ae19c
Chad Jones <chadjones839@gmail.com>
2020-08-25 14:04:15 -0500

    Phase 5 Completed

--- Program.cs
+++ Program.cs
@@ -11,34 +11,41 @@ namespace GuessingGame
 
         static void GuessNumber()
         {
-            Console.Write("Guess the Secret Number!! ");
-            string answer = Console.ReadLine();
-            string secretNumber = "42";
+            Console.Write("Guess the Secret Number!! ---> ");
+            string answerString = Console.ReadLine();
+            int answer = Int32.Parse(answerString);
+            int secretNumber = new Random().Next(1,100);
             int count = 0;
+            int countdown = 4;
 
             while (answer != secretNumber && ++count < 4)
             {
+            if (answer != secretNumber)
                 Console.Write("WRONG!! ");
                 Console.Write(answer);
-                Console.Write(" isn't even close! ");
-                Console.Write("Guess again! ");
-                answer = Console.ReadLine();
+                Console.Write($" isn't even close! You have {--countdown} guesses left!");
+                Console.WriteLine();
+                Console.Write("Guess Again ---> ");
+                answerString = Console.ReadLine();
+                Console.WriteLine();
+
+                answer = Int32.Parse(answerString);
             }
             if (answer == secretNumber)
             {
-                Console.WriteLine();
                 Console.WriteLine("-----------------------------------------------------------------");
                 Console.WriteLine();
                 Console.Write("You got it! What, do you want a ribbon or something? Now get lost! ");
             }
             else
             {
-                Console.WriteLine();
                 Console.WriteLine("---------------------------------");
                 Console.WriteLine("YOU BLEW IT, TURKEY!");
                 Console.WriteLine();
+                Console.WriteLine($"--> The answer was {secretNumber} <--");
+                Console.WriteLine();
                 Console.WriteLine(@"""Nobody likes a sore loser.""");
-                Console.WriteLine("            -Abraham Lincoln");
+                Console.WriteLine("               -Abraham Lincoln");
             }
         }
 
