fe08fbd797fb8b4496fed959068a6900dfeb60ab
Chad Jones <chadjones839@gmail.com>
2020-08-25 10:52:26 -0500

    Phase 1 Completed

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GuessNumber();
+        }
+
+        static void GuessNumber()
+        {
+            Console.Write("Guess the Secret Number!! ");
+            string answer = Console.ReadLine();
+            Console.Write("You Guessed ");
+            Console.Write(answer);
+        }
+
+    }
+}
